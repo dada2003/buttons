997564764f19ac466fbfd6954cb7a7503957a22b
Unknown <audees10@gmail.com>
2017-07-21 22:27:18 +0300

    change name of the button

--- js/script.js
+++ js/script.js
@@ -1,34 +1,39 @@
-function showMsg(){
-   alert('hallo');
+function showMsg() {
+    alert('hallo');
 }
 var first = document.getElementsByName('first');
-first[0].addEventListener('click',showMsg);
+first[0].addEventListener('click', showMsg);
 
 
 
-function showConsole(){
-   console.log('hallo');
+function showConsole() {
+    console.log('hallo');
 }
 var second = document.getElementsByName('second');
-second[0].addEventListener('click',showConsole);
+second[0].addEventListener('click', showConsole);
 
 
 
 
 var parentElem = document.body;
-function addSpan(){
- var span = document.createElement('span');
-  span.className = "alert alert-success";
-  span.innerHTML = "Hello";
 
-  document.body.appendChild(span);
+function addSpan() {
+    var span = document.createElement('span');
+    span.className = "alert alert-success";
+    span.innerHTML = "Hello";
+
+    document.body.appendChild(span);
 }
 
 var third = document.getElementsByName('third');
-third[0].addEventListener('click',addSpan);
+third[0].addEventListener('click', addSpan);
 
 
 
+var prom = prompt('write something');
 
-
-
+function setValue() {
+    document.body.children[3].value = prom;
+}
+var fourth = document.getElementsByName('fourth');
+fourth[0].addEventListener('click', setValue);
